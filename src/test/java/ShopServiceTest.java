c200aa12d67ed2cd34c14894dae023d47687902a
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-09-18 09:30:51 +0200

    Order status test added.

--- src/test/java/ShopServiceTest.java
+++ src/test/java/ShopServiceTest.java
@@ -17,8 +17,22 @@ class ShopServiceTest {
 
         //THEN
         Order expected = new Order("-1", List.of(new Product("1", "Apfel")));
+        assertEquals(expected.getStatus(), actual.getStatus());
         assertEquals(expected.getProducts(), actual.getProducts());
         assertNotNull(expected.getId());
+    }
+
+    @Test
+    void addOrderTest_defaultStatus() {
+        //GIVEN
+        ShopService shopService = new ShopService();
+        List<String> productsIds = List.of("1");
+
+        //WHEN
+        Order actual = shopService.addOrder(productsIds);
+
+        //THEN
+        assertEquals(OrderStatus.PROCESSING, actual.getStatus());
     }
 
     @Test
