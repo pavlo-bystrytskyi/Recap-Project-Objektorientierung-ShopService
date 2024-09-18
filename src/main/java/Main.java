ca886ce648e52990abc9efe60645b32ac779fd94
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-09-18 11:20:40 +0200

    Added IdService.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         ProductRepo productRepo = new ProductRepo();
         OrderRepo orderRepo = new OrderMapRepo();
-        ShopService shopService = new ShopService(productRepo, orderRepo);
+        IdService idService = new IdService();
+        ShopService shopService = new ShopService(productRepo, orderRepo, idService);
 
         shopService.addOrder(List.of("1"));
         shopService.addOrder(List.of("2"));
--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -4,12 +4,12 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 public class ShopService {
     private final ProductRepo productRepo;
     private final OrderRepo orderRepo;
+    private final IdService idService;
 
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
@@ -21,7 +21,7 @@ public class ShopService {
             products.add(productToOrder.get());
         }
 
-        Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING, Instant.now());
+        Order newOrder = new Order(idService.generateId(), products, OrderStatus.PROCESSING, Instant.now());
 
         return orderRepo.addOrder(newOrder);
     }
