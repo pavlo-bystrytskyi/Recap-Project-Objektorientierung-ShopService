efb2d1583f31616bdfbdd3a7847520e2e3e36c64
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-09-18 10:58:53 +0200

    Field that stores the order timestamp added.

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,19 +1,27 @@
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NonNull;
-import lombok.With;
+import lombok.*;
 
+import java.time.Instant;
 import java.util.List;
 
 @AllArgsConstructor
+@EqualsAndHashCode(exclude = {"placedAt"})
 @With
 @Data
 public class Order {
-        final @NonNull String id;
-        final @NonNull List<Product> products;
-        @NonNull OrderStatus status;
+    final @NonNull String id;
+    final @NonNull List<Product> products;
+    final @NonNull OrderStatus status;
+    final @NonNull Instant placedAt;
 
-        public Order(String id, List<Product> products) {
-            this(id, products, OrderStatus.PROCESSING);
-        }
+    public Order(String id, List<Product> products) {
+        this(id, products, OrderStatus.PROCESSING, Instant.now());
+    }
+
+    public Order(String id, List<Product> products, Instant placedAt) {
+        this(id, products, OrderStatus.PROCESSING, placedAt);
+    }
+
+    public Order(String id, List<Product> products, OrderStatus orderStatus) {
+        this(id, products, orderStatus, Instant.now());
+    }
 }
