c50edfbdc11582eab28d3135714cc1d0456e78cc
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-09-18 10:00:00 +0200

    Optional Product added.

--- src/test/java/ProductRepoTest.java
+++ src/test/java/ProductRepoTest.java
@@ -27,7 +27,7 @@ class ProductRepoTest {
         ProductRepo repo = new ProductRepo();
 
         //WHEN
-        Product actual = repo.getProductById("1");
+        Product actual = repo.getProductById("1").get();
 
         //THEN
         Product expected = new Product("1", "Apfel");
@@ -46,7 +46,7 @@ class ProductRepoTest {
         //THEN
         Product expected = new Product("2", "Banane");
         assertEquals(actual, expected);
-        assertEquals(repo.getProductById("2"), expected);
+        assertEquals(repo.getProductById("2").get(), expected);
     }
 
     @org.junit.jupiter.api.Test
@@ -58,6 +58,6 @@ class ProductRepoTest {
         repo.removeProduct("1");
 
         //THEN
-        assertNull(repo.getProductById("1"));
+        assertTrue(repo.getProductById("1").isEmpty());
     }
 }
